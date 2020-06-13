c9998549ef02b63dc09eac964d4f5de2994398b8
pr4th4m <pr4th4m9@gmail.com>
2020-06-13 14:43:22 +1000

    Add webpack

--- src/command.ts
+++ src/command.ts
@@ -46,7 +46,7 @@ export const requestHandler = async () => {
 
   channel.clear();
   channel.show();
-  channel.append(`Waiting for response from ${httpRequest.url}`);
+  channel.append(`Requesting ${httpRequest.url}`);
   const channelBuffer = `output:///${channel.name}`;
 
   let httpClient = new HttpClient(config, document);
@@ -106,49 +106,3 @@ export const requestHandler = async () => {
     }
   }
 };
-
-// let selectedText = text;
-// let selectedText = getDelimitedText(text, pos.line);
-// console.log(selectedText);
-
-// let matched = selectedText.match(RequestVariableDefinitionRegex);
-// console.log(matched);
-
-// const rawLines = selectedText
-//   .split(LineSplitterRegex)
-//   .filter((l) => !CommentIdentifiersRegex.test(l));
-// // console.log(rawLines);
-//
-// const requestRange = getRequestRanges(rawLines)[0];
-// // console.log(requestRange);
-//
-// selectedText = rawLines.slice(requestRange[0], requestRange[1] + 1).join(EOL);
-// // console.log(selectedText);
-//
-// parseHttpRequest(selectedText);
-
-// const requestRange = this.getRequestRanges(rawLines)[0];
-// if (!requestRange) {
-//   return null;
-// }
-
-// let range = await workspace.getSelectedRange("v", document);
-// console.log(range);
-
-// let wins = await workspace.nvim.windows;
-// console.log(wins.length);
-// let buf = await workspace.nvim.createNewBuffer(true, true);
-// buf.insert("love you", 0);
-
-// let state = await workspace.getCurrentState();
-// console.log(state.document);
-// console.log(state.position);
-
-// let channel = workspace.createOutputChannel("http");
-// channel.show();
-// channel.append(data);
-
-// let win = await workspace.nvim.createWindow(0);
-// let buf = await win.buffer;
-// buf.insert("love you", 0);
-// buf.append("love you");
